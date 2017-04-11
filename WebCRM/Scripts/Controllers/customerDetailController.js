7631b34df95736eba75b03c1002feef000524177
Ray <rayhsu02@gmail.com>
2017-04-10 19:11:27 -0700

    commit

---
+++ WebCRM/Scripts/Controllers/customerDetailController.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('customerDetailController', customerDetailController);
+
+    customerDetailController.$inject = ['$scope', 'crmService', '$state'];
+
+    function customerDetailController($scope, crmService, $state) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'customerDetailController';
+        console.log('$state.params.selectedCustomer');
+        console.log($state.params.selectedCustomer);
+        $scope.newCompany = angular.copy($state.params.selectedCustomer);
+        $scope.updateCustomer = updateCustomer;
+        $scope.IndustryTypes = [{ Name: 'Health Care', Id: '1' }];
+
+        //activate();
+
+        //function activate() { }
+
+        function updateCustomer(newCompany) {
+
+            console.log('updateCustomer');
+            console.log(newCompany);
+
+            if (newCompany.customerId != undefined) {
+
+                crmService.updateCustomer(newCompany)
+                    .then(function (res) {
+                        console.log(res);
+                        $state.go('CustomerList');
+
+                    }, function () {
+                        alert(res);
+                    });
+            } 
+
+        } 
+    }
+})();
